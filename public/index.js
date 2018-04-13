92827ee987cc187b25c4cfc96692f6399dae32f0
Brian <riceb53@gmail.com>
2018-04-12 21:08:58 -0500

    make incomplete tasks counter

--- public/index.js
+++ public/index.js
@@ -5,13 +5,47 @@ var HomePage = {
   data: function() {
     return {
       message: "Welcome to Vue.js!",
-      task: "go to the store",
-      task2: "get a haircut",
-      task3: "do laundry"
+      tasks: [
+        {text: 'Take out the garbage', completed: false},
+        {text: 'Make the bed', completed: false},
+        {text: 'Mow the lawn', completed: false}
+      ]
     };
   },
   created: function() {},
-  methods: {},
+  methods: {
+    removeCompletedTasks: function() {
+      // get all the tasks
+      // filter out the incomplete ones
+
+      var incompleteTasks = [];
+      for (var i = 0; i < this.tasks.length; i++) {
+        if (this.tasks[i].completed === false) {
+          // shovel into the incomplete tasks array
+          incompleteTasks.push(this.tasks[i]);
+        }
+      }
+      this.tasks = incompleteTasks;
+    },
+    numberOfIncompletedTasks: function() {
+      // count the number of tasks that are incomplete
+      // get all the tasks
+      // this.tasks
+      // figure out which ones are incomplete
+      var counter = 0;
+      for (var i = 0; i < this.tasks.length; i++) {
+        if (this.tasks[i].completed === false) {
+          counter++;
+        }
+      }
+      // count em
+      return counter;
+
+    },
+    toggleCompleted: function(theTask) {
+      theTask.completed = !theTask.completed;
+    }
+  },
   computed: {}
 };
 
